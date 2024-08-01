71d4ffb7eeebfb517e26f0bdaf14a068ed18d855
bg-miles <piuschukwuebuka40@gmail>
2024-08-01 23:30:04 +0100

    {'concat': <class 'commit'>}'

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Contains the concat Module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concats two strings
+        Return: string
+    """
+    return str1 + str2
