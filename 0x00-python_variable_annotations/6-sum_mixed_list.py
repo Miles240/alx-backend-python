33d50055560cf494b8b6f58294d0750e4913a2a9
bg-miles <piuschukwuebuka40@gmail>
2024-08-02 01:49:30 +0100

    Listg

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """contains the mxd_lst module"""
-from typing import Union
+from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
