58512cd5e286b7850548c5d2a6462ee2e2433c34
bg-miles <piuschukwuebuka40@gmail>
2024-08-02 01:39:06 +0100

    list - floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""contains the sum_list module"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum list function
+            Returns: Float
+    """
+    return sum(input_list)
