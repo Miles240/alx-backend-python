c2cac7cd3ba87dee3fa27c38b4e2aa0a385a447c
bg-miles <piuschukwuebuka40@gmail>
2024-08-02 01:44:05 +0100

    list - mixed

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""contains the mxd_lst module"""
+from typing import Union
+
+
+def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+    """sums a mixed list function
+            Returns: Float
+    """
+    return sum(mxd_lst)
